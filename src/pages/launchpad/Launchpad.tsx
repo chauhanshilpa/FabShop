611bcb501d0d035f4951ed9a58a58280c40f5e22
Shilpa Chauhan <101130846+chauhanshilpa@users.noreply.github.com>
2025-01-11 02:13:43 +0530

    Add verified icon, refresh input and auto fill subCategory based on category value in launchpad

--- src/pages/launchpad/Launchpad.tsx
+++ src/pages/launchpad/Launchpad.tsx
@@ -15,6 +15,8 @@ import Button from "@mui/material/Button";
 import { addNewProduct } from "../../api/api";
 import balloonGif from "../../api/assets/launch-successfull.gif";
 import cheerAudio from "../../api/assets/cheering-claps.mp3";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import { Translate } from "@mui/icons-material";
 interface Props {
   refreshProducts: () => Promise<void>;
 }
@@ -35,6 +37,7 @@ const Launchpad = ({ refreshProducts }: Props) => {
   const [browsedImage, setBrowsedImage] = useState<File | null>(null);
   const [imageUrlActiveField, setImageUrlActiveField] = useState("");
   const [launchSuccessfull, setLauchSuccessfull] = useState(false);
+  const [showVerifiedIcon, setShowVerifiedIcon] = useState(false);
 
   useEffect(() => {
     if (productType) {
@@ -42,11 +45,38 @@ const Launchpad = ({ refreshProducts }: Props) => {
     }
   }, [productType]);
 
+  useEffect(() => {
+    if (categoryInputValue === "Men") {
+      setSubCategoryInputValue("Boys");
+      setSubCategory("Boys");
+    } else if (categoryInputValue === "Women") {
+      setSubCategoryInputValue("Girls");
+      setSubCategory("Girls");
+    }
+  }, [category, categoryInputValue]);
+
   const handleChange = (newValue: File | null) => {
     setBrowsedImage(newValue);
     const fileUrl = URL.createObjectURL(newValue as File);
     setImageUrl(fileUrl);
   };
+
+  const refreshInputsToInitialState = () => {
+    setCategory(null);
+    setCategoryInputValue("");
+    setSubCategory(null);
+    setSubCategoryInputValue("");
+    setName("");
+    setProductType(null);
+    setProductTypeTitle("");
+    setPrice("");
+    setDescription("");
+    setImageUrl("");
+    setBrowsedImage(null);
+  };
+
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
 
   const launchProduct = async () => {
     await addNewProduct(
@@ -58,150 +88,183 @@ const Launchpad = ({ refreshProducts }: Props) => {
       name,
       description
     );
-    setLauchSuccessfull(true);
-    let audio = new Audio(cheerAudio)
-    audio.play();
-    setTimeout(()=>{
-    setLauchSuccessfull(false);
-    audio.pause();
-    },3000)
-    //
     await refreshProducts();
+    setLauchSuccessfull(true);
+    let audio = new Audio(cheerAudio);
+    audio.play();
+
+    await delay(2000);
+    // these three line will run after a delay of 2 second
+    setLauchSuccessfull(false);
+    refreshInputsToInitialState();
+    setShowVerifiedIcon(true);
+
+    // these two line will run after a delay of 1 second after above is resolved
+    await delay(1000);
+    audio.pause();
+
+    // these two line will run after a delay of 2 second after above is resolved
+    await delay(1000);
+    setShowVerifiedIcon(false);
   };
 
   return (
-    <Box className="main launchpad">
-      <Typography variant="h4" className="launchpad-heading">
-        Launch New Product
-      </Typography>
-      <Box className="input-line-1">
-        <Autocomplete
-          className="input"
-          value={category}
-          onChange={(event: any, newValue: string | null) => {
-            setCategory(newValue);
-          }}
-          inputValue={categoryInputValue}
-          onInputChange={(event, newInputValue) => {
-            setCategoryInputValue(newInputValue);
-          }}
-          id="controllable-states-demo"
-          options={CATEGORY_LIST}
-          sx={{ width: 300 }}
-          renderInput={(params) => (
-            <TextField {...params} label="Choose category" color="success" />
-          )}
-        />
-        <Autocomplete
-          className="input"
-          value={subCategory}
-          onChange={(event: any, newValue: string | null) => {
-            setSubCategory(newValue);
-          }}
-          inputValue={subCategoryInputValue}
-          onInputChange={(event, newInputValue) => {
-            setSubCategoryInputValue(newInputValue);
-          }}
-          id="controllable-states-demo"
-          options={SUB_CATEGORY_LIST}
-          sx={{ width: 300 }}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Choose sub-category"
-              color="success"
-            />
-          )}
-        />
-        <AutocompleteAndAddNewInput
-          productType={productType}
-          setProductType={setProductType}
-        />
+    <>
+      <Box className="main launchpad">
+        {showVerifiedIcon && (
+          <VerifiedIcon
+            className="verified-icon"
+            sx={{
+              position: "absolute",
+              left: "44%",
+              top: "60%",
+              transform: "translate(-60%, -44%)",
+              zIndex: "99",
+            }}
+          />
+        )}
+        <Typography variant="h4" className="launchpad-heading">
+          Launch New Product
+        </Typography>
+        <Box className="input-line-1">
+          <Autocomplete
+            className="input"
+            value={category}
+            onChange={(event: any, newValue: string | null) => {
+              setCategory(newValue);
+            }}
+            inputValue={categoryInputValue}
+            onInputChange={(event, newInputValue) => {
+              setCategoryInputValue(newInputValue);
+            }}
+            id="controllable-states-demo"
+            options={CATEGORY_LIST}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Choose category" color="success" />
+            )}
+          />
+          <Autocomplete
+            className="input"
+            value={subCategory}
+            onChange={(event: any, newValue: string | null) => {
+              setSubCategory(newValue);
+            }}
+            inputValue={subCategoryInputValue}
+            onInputChange={(event, newInputValue) => {
+              setSubCategoryInputValue(newInputValue);
+            }}
+            id="controllable-states-demo launchpad_subCategoryInput"
+            options={SUB_CATEGORY_LIST}
+            sx={{ width: 300 }}
+            disabled={
+              subCategory !== null &&
+              subCategoryInputValue !== "" &&
+              category !== "Kids" &&
+              categoryInputValue !== "Kids" && category !== null && category !== "" 
+                ? true
+                : false
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Choose sub-category"
+                color="success"
+              />
+            )}
+          />
+          <AutocompleteAndAddNewInput
+            productType={productType}
+            setProductType={setProductType}
+          />
+        </Box>
+        <Box className="input-line-2">
+          <TextField
+            className="input"
+            required
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            color="success"
+          />
+          <TextField
+            className="input"
+            required
+            id="outlined-basic"
+            label="Price"
+            variant="outlined"
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+            color="success"
+          />
+        </Box>
+        <Box className="input-line-3">
+          <MuiFileInput
+            value={browsedImage}
+            onChange={handleChange}
+            placeholder="&#128194;&nbsp;Insert a file"
+            className="browse-file"
+            color="success"
+            style={{
+              filter:
+                imageUrlActiveField === "textField" ? "blur(2px)" : "none",
+            }}
+            onClick={() => setImageUrlActiveField("fileInput")}
+          />
+          <TextField
+            sx={{ width: "60%" }}
+            className="input"
+            id="outlined-required"
+            label="Image url"
+            value={imageUrl}
+            color="success"
+            style={{
+              filter:
+                imageUrlActiveField === "fileInput" ? "blur(2px)" : "none",
+            }}
+            onChange={(event) => {
+              setImageUrl(event.target.value);
+              setBrowsedImage(null);
+            }}
+            onClick={() => setImageUrlActiveField("textField")}
+          />
+        </Box>
+        <Box className="input-line-4">
+          <TextField
+            sx={{ width: "70%" }}
+            className="input"
+            id="outlined-multiline-static"
+            label="Description"
+            multiline
+            rows={4}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            defaultValue="This is a sample description"
+            color="success"
+          />
+        </Box>
+        {launchSuccessfull && (
+          <img
+            src={balloonGif}
+            alt="balloons for cheering successfull product launch"
+            style={{
+              position: "absolute",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "30%",
+            }}
+            className="success_launch_image"
+          />
+        )}
+        <Box className="launch-button-Box">
+          <Button className="launch-button" onClick={launchProduct}>
+            Launch Now&nbsp;🚀
+          </Button>
+        </Box>
       </Box>
-      <Box className="input-line-2">
-        <TextField
-          className="input"
-          required
-          id="outlined-basic"
-          label="Name"
-          variant="outlined"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-          color="success"
-        />
-        <TextField
-          className="input"
-          required
-          id="outlined-basic"
-          label="Price"
-          variant="outlined"
-          value={price}
-          onChange={(event) => setPrice(event.target.value)}
-          color="success"
-        />
-      </Box>
-      <Box className="input-line-3">
-        <MuiFileInput
-          value={browsedImage}
-          onChange={handleChange}
-          placeholder="&#128194;&nbsp;Insert a file"
-          className="browse-file"
-          color="success"
-          style={{
-            filter: imageUrlActiveField === "textField" ? "blur(2px)" : "none",
-          }}
-          onClick={() => setImageUrlActiveField("fileInput")}
-        />
-        <TextField
-          sx={{ width: "60%" }}
-          className="input"
-          id="outlined-required"
-          label="Image url"
-          value={imageUrl}
-          color="success"
-          style={{
-            filter: imageUrlActiveField === "fileInput" ? "blur(2px)" : "none",
-          }}
-          onChange={(event) => {
-            setImageUrl(event.target.value);
-            setBrowsedImage(null);
-          }}
-          onClick={() => setImageUrlActiveField("textField")}
-        />
-      </Box>
-      <Box className="input-line-4">
-        <TextField
-          sx={{ width: "70%" }}
-          className="input"
-          id="outlined-multiline-static"
-          label="Description"
-          multiline
-          rows={4}
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          defaultValue="This is a sample description"
-          color="success"
-        />
-      </Box>
-      {launchSuccessfull && (
-        <img
-          src={balloonGif}
-          alt="balloons for cheering successfull product launch"
-          style={{
-            position: "absolute",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: "30%",
-          }}
-          className="success_launch_image"
-        />
-      )}
-      <Box className="launch-button-Box">
-        <Button className="launch-button" onClick={launchProduct}>
-          Launch Now&nbsp;🚀
-        </Button>
-      </Box>
-    </Box>
+    </>
   );
 };
 
